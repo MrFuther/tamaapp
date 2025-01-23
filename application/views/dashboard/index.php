4c21da7693b5c3c459fe3db94a426844194d2135
EleanorGracieneda <eleanor.gracieneda@gmail.com>
2025-01-23 14:59:45 +0700

    chart.js

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -213,6 +213,8 @@
         );
     }
 
+    
+
     // Render Components
     ReactDOM.render(<LineChartComponent />, document.getElementById("line-chart-root"));
     ReactDOM.render(<SegmentationChartComponent />, document.getElementById("segmentation-chart-root"));
