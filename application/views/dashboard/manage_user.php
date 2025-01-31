de51875fbab1e437978ac8ff56ee87543eda1e8a
Laodeadil <adilwahyudiiii@gmail.com>
2025-01-31 11:07:53 +0700

    Update

--- application/views/dashboard/manage_user.php
+++ application/views/dashboard/manage_user.php
@@ -224,6 +224,7 @@
             </div>
         </div>
     </div>
+    
     <!-- Logout Confirmation Modal -->
     <div class="modal fade" id="logoutModal" tabindex="-1" aria-labelledby="logoutModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered modal-confirm">
