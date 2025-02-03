5affcdaa24d9b55baaaf9481e6ca826b2c3ee747
MrFuther <Washingtonfrank033@gmail.com>
2025-02-03 12:03:09 +0700

    apasih

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -304,11 +304,11 @@
                   <div class="weather-info">
                     <div class="d-flex">
                       <div>
-                        <h2 class="mb-0 font-weight-normal"><i class="icon-sun mr-2"></i>31<sup>C</sup></h2>
+                        <h2 class="mb-0 font-weight-normal"><i class="icon-sun mr-2"></i>28<sup>C</sup></h2>
                       </div>
                       <div class="ml-2">
-                        <h4 class="location font-weight-normal">Bangalore</h4>
-                        <h6 class="font-weight-normal">India</h6>
+                        <h4 class="location font-weight-normal">Tangerang</h4>
+                        <h6 class="font-weight-normal">Indonesia</h6>
                       </div>
                     </div>
                   </div>
