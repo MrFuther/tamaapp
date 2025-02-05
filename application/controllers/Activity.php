76a9cdcc581fd19b05158300f38f8f2744d1857f
MrFuther <Washingtonfrank033@gmail.com>
2025-02-05 10:06:20 +0700

    Print PDF

--- application/controllers/Activity.php
+++ application/controllers/Activity.php
@@ -12,6 +12,7 @@ class Activity extends CI_Controller
         $this->load->model('ActivityModel');
         $this->load->model('MasterDataModel');
         $this->load->library('upload'); // Load library untuk upload file
+        $this->load->library('tcpdf');
     }
 
     public function index()
@@ -75,6 +76,71 @@ class Activity extends CI_Controller
     $device_type = $this->input->get('device_type');
     $devices = $this->ActivityModel->getDevicesByType($device_type);
     echo json_encode($devices);
+    }
+
+    public function print_pdf($id) {
+        // Ambil data untuk PDF (modifikasi sesuai dengan logika aplikasi Anda)
+        $activity = $this->ActivityModel->get_activity_by_id($id);
+    
+        // Membuat instance PDF baru
+        $pdf = new Pdf(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+
+        // Set document information
+        $pdf->SetCreator(PDF_CREATOR);
+        $pdf->SetAuthor('BSH');
+        
+        // Pastikan tanggal tidak null sebelum diproses
+        $doc_date = !empty($activity['tanggal']) ? date('d/m/Y', strtotime($activity['tanggal'])) : 'N/A';
+        $pdf->SetTitle('Form PM - ' . $doc_date);
+
+        // Set margins
+        $pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+        $pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
+
+        // Set auto page breaks
+        $pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
+
+        // Add a page
+        $pdf->AddPage();
+
+        // Set font
+        $pdf->SetFont('helvetica', '', 11);
+
+        // Add content from database
+        $pdf->Ln(10);
+        $pdf->Cell(50, 8, 'Tanggal / Bulan / Tahun:', 0, 0);
+        $pdf->Cell(0, 8, $doc_date, 0, 1);
+
+        $pdf->Cell(50, 8, 'Lokasi:', 0, 0);
+        $pdf->Cell(0, 8, !empty($activity['lokasi']) ? $activity['lokasi'] : 'N/A', 0, 1);
+
+        $pdf->Cell(50, 8, 'Perangkat:', 0, 0);
+        $pdf->Cell(0, 8, !empty($activity['device']) ? $activity['device'] : 'N/A', 0, 1);
+
+        $pdf->Cell(50, 8, 'Shift:', 0, 0);
+        $pdf->Cell(0, 8, !empty($activity['shift']) ? $activity['shift'] : 'N/A', 0, 1);
+
+        $pdf->Cell(50, 8, 'Personil:', 0, 0);
+        $pdf->Cell(0, 8, !empty($activity['personil']) ? $activity['personil'] : 'N/A', 0, 1);
+
+        $pdf->Ln(10);
+        
+        $pdf->Cell(60, 10, 'Foto Perangkat', 1, 0, 'C');
+        $pdf->Cell(60, 10, 'Foto Lokasi', 1, 0, 'C');
+        $pdf->Cell(60, 10, 'Foto Teknisi', 1, 1, 'C');
+        $pdf->Cell(60, 30, 'Foto', 1, 0, 'C');
+        $pdf->Cell(60, 30, 'Foto', 1, 0, 'C');
+        $pdf->Cell(60, 30, 'Foto', 1, 1, 'C');
+        $pdf->Cell(60, 10, 'Deskripsi', 1, 0, 'C');
+        $pdf->Cell(60, 10, 'Deskripsi', 1, 0, 'C');
+        $pdf->Cell(60, 10, 'Deskripsi', 1, 1, 'C');
+        // Add images
+
+        // Generate filename
+        $filename = 'form_pm_' . (!empty($activity['tanggal']) ? date('Ymd', strtotime($activity['tanggal'])) : 'undated');
+        
+        // Output the PDF
+        $pdf->Output($filename . '.pdf', 'I');
     }
 
     private function _uploadFile($fieldName)
