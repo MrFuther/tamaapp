d6e99642a34323c41fa1002305bc9cb7894d0dda
MrFuther <Washingtonfrank033@gmail.com>
2025-02-06 09:36:44 +0700

    Navbar and Sidebar Fixed

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -221,45 +221,7 @@
         </div>
       </div>
       <!-- partial:partials/_sidebar.html -->
-        <nav class="sidebar sidebar-offcanvas" id="sidebar">
-            <ul class="nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="<?php echo base_url('dashboard'); ?>">
-                    <i class="icon-grid menu-icon"></i>
-                    <span class="menu-title">Dashboard</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="<?php echo base_url('activity'); ?>">
-                    <i class="icon-grid menu-icon"></i>
-                    <span class="menu-title">Activity</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" data-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
-                    <i class="icon-grid menu-icon"></i>
-                    <span class="menu-title">Master Data</span>
-                    <i class="menu-arrow"></i>
-                    </a>
-                    <div class="collapse" id="auth">
-                    <ul class="nav flex-column sub-menu">
-                        <li class="nav-item"> <a class="nav-link" href="<?php echo base_url('unitkerja'); ?>"> Master Unit Kerja </a></li>
-                        <li class="nav-item"> <a class="nav-link" href="<?php echo base_url('subunitkerja'); ?>"> Master Sub Unit </a></li>
-                        <li class="nav-item"> <a class="nav-link" href="<?php echo base_url('grouparea'); ?>"> Master Group Area </a></li>
-                        <li class="nav-item"> <a class="nav-link" href="<?php echo base_url('subarea'); ?>"> Master Sub Area </a></li>
-                        <li class="nav-item"> <a class="nav-link" href="<?php echo base_url('groupdevice'); ?>"> Master Group Device </a></li>
-                        <li class="nav-item"> <a class="nav-link" href="<?php echo base_url('subdevice'); ?>"> Master Group Area </a></li>
-                    </ul>
-                    </div>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="<?php echo base_url('m_user'); ?>">
-                    <i class="icon-head menu-icon"></i>
-                    <span class="menu-title">Manage User</span>
-                    </a>
-                </li>
-            </ul>
-        </nav>
+      <?php include 'sidebar.php'; ?>
       <!-- partial -->
       <div class="main-panel">
         <div class="content-wrapper">
