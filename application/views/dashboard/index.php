b22943487e7860d91eb1653a4192bb3c32c39b96
EleanorGracieneda <eleanor.gracieneda@gmail.com>
2025-02-18 15:57:21 +0700

    AMAR

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TAMA Apps</title>
+    <title>AMAR Apps</title>
     <!-- plugins:css -->
   
     <link rel="stylesheet" href="<?php echo base_url('vendors/feather/feather.css'); ?>">
