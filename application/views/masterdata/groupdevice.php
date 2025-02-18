ba5f422a0da90cb61e5dc96aa0171d1126d10da9
EleanorGracieneda <eleanor.gracieneda@gmail.com>
2025-02-18 13:45:31 +0700

    devicehidnnew

--- application/views/masterdata/groupdevice.php
+++ application/views/masterdata/groupdevice.php
@@ -25,11 +25,8 @@
 </head>
 <body>
   <div class="container-scroller">
-    
     <!-- partial:partials/_navbar.html -->
     <?php include APPPATH . 'views\dashboard\navbar.php'; ?>
-    <?php include APPPATH . 'views\dashboard\footer.php'; ?>
-    <?php include APPPATH . 'views\dashboard\sidebar.php'; ?>
     
     <!-- partial -->
     <div class="container-fluid page-body-wrapper">
@@ -54,6 +51,15 @@
       </div>
       
       <div id="right-sidebar" class="settings-panel">
+        <i class="settings-close ti-close"></i>
+        <ul class="nav nav-tabs border-top" id="setting-panel" role="tablist">
+          <li class="nav-item">
+            <a class="nav-link active" id="todo-tab" data-toggle="tab" href="#todo-section" role="tab" aria-controls="todo-section" aria-expanded="true">TO DO LIST</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" id="chats-tab" data-toggle="tab" href="#chats-section" role="tab" aria-controls="chats-section">CHATS</a>
+          </li>
+        </ul>
         <div class="tab-content" id="setting-content">
           <div class="tab-pane fade show active scroll-wrapper" id="todo-section" role="tabpanel" aria-labelledby="todo-section">
             <div class="add-items d-flex px-3 mb-0">
@@ -70,6 +76,9 @@
           <!-- To do section tab ends -->
         </div>
       </div>
+      <!-- partial -->
+      <!-- partial:partials/_sidebar.html -->
+      <?php include APPPATH . 'views\dashboard\sidebar.php'; ?>
       <!-- partial -->
       <div class="main-panel">
       <div class="content-wrapper">
@@ -171,7 +180,42 @@
         <!-- partial -->
       </div>
       <!-- main-panel ends -->
-    
+    </div>
+    <div id="logoutModal" class="modal" tabindex="-1" role="dialog">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">Konfirmasi Logout</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <p>Apakah Anda yakin ingin logout?</p>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+            <button type="button" class="btn btn-primary" onclick="logout()">Logout</button>
+          </div>
+        </div>
+      </div>
+    </div>  
+    <!-- page-body-wrapper ends -->
+  </div>
+  <!-- container-scroller -->
+  <script>
+    function showLogoutConfirmation() {
+    // Menampilkan modal
+    var modal = new bootstrap.Modal(document.getElementById('logoutModal'));
+    modal.show();
+    }
+
+    function logout() {
+    // Implementasi logika logout di sini
+    alert("Anda telah logout.");
+    // Redirect ke halaman login atau halaman utama setelah logout
+    window.location.href = "<?php echo base_url('auth/logout'); ?>";  // Ganti dengan URL halaman login Anda
+    }
   </script>
   <!-- plugins:js -->
   <script src="<?php echo base_url('vendors/js/vendor.bundle.base.js'); ?>"></script>
