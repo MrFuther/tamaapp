3b775e37e841f5218ba4472293dae97896e39fc2
MrFuther <Washingtonfrank033@gmail.com>
2025-02-27 09:50:49 +0700

    fixed activity

--- application/views/dashboard/activity.php
+++ application/views/dashboard/activity.php
@@ -29,41 +29,6 @@
 <?php include 'navbar.php'; ?>
   <!-- Wrapper utama untuk halaman -->
 <div class="container-fluid page-body-wrapper">
-
-<!-- Panel Pengaturan Tema -->
-<div class="theme-setting-wrapper">
-  <div id="theme-settings" class="settings-panel">
-    <i class="settings-close ti-close"></i>
-    
-    <!-- Pengaturan Header -->
-    <p class="settings-heading mt-2">HEADER SKINS</p>
-      <div class="color-tiles mx-0 px-4">
-        <div class="tiles success"></div>
-        <div class="tiles warning"></div>
-        <div class="tiles danger"></div>
-        <div class="tiles info"></div>
-        <div class="tiles dark"></div>
-        <div class="tiles default"></div>
-      </div>
-    </div>
-  </div>
-
-  <!-- Sidebar Kanan untuk To-Do List dan Chat -->
-  <div id="right-sidebar" class="settings-panel">
-    <i class="settings-close ti-close"></i>
-
-        
-        <div class="tab-content" id="setting-content">
-          <div class="tab-pane fade show active scroll-wrapper" id="todo-section" role="tabpanel" aria-labelledby="todo-section">
-            <div class="add-items d-flex px-3 mb-0">
-             
-            </div>
-            <div class="list-wrapper px-3">
-                </div>
-          </div>
-          <!-- To do section tab ends -->
-        </div>
-      </div>
       <!-- partial -->
       <!-- partial:partials/_sidebar.html -->
       <?php include 'sidebar.php'; ?>
